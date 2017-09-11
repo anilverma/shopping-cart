54512d53f208ef9d7bbb47d5a0144907340f59e2
averma6 <anil.verma@searshc.com>
2017-09-11 18:02:16 +0530

    removed

--- src/main/java/com/shoppingCart/app/dao/OrderDao.java
+++
@@ -1,9 +0,0 @@
-package com.shoppingCart.app.dao;
-
-import com.shoppingCart.app.model.Order;
-
-public interface OrderDao {
-
-	Long save(Order order);
-
-}
--- src/main/java/com/shoppingCart/app/util/OrderStatus.java
+++
@@ -1,7 +0,0 @@
-package com.shoppingCart.app.util;
-
-public enum OrderStatus {
-
-	NEW, HOLD, SHIPPED, DELIVERED, CLOSED;
-
-}
